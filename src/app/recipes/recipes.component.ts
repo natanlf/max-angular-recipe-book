388228e02f82a619e96b6c4857c7a150886260c1
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2022-03-22 09:12:20 -0300

    Using a Service for Cross-Component Communication

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,3 +1,4 @@
+import { RecipeService } from './recipe.service';
 import { Recipe } from './recipe.model';
 import { Component, OnInit } from '@angular/core';
 
@@ -10,9 +11,13 @@ export class RecipesComponent implements OnInit {
 
   selectedRecipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipeService.recipeSelected
+    .subscribe( (recipe: Recipe) => {
+      this.selectedRecipe = recipe;
+    });
   }
 
 }
